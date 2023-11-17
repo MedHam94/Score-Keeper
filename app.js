34fa9484aa20ad581fd5499d300baebe7dba96cf
MedHam94 <hammas.m.mh@gmail.com>
2023-11-17 19:57:39 +0100

    refactoring the code

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
 // Declare variables
+const p1 = {
+    button :document.querySelector('#p1Button'),
+    display: document.querySelector('#p1Display'),
+    score:0
+}
 
-const p1Button = document.querySelector('#p1Button')
+const p2 = {
+    button :document.querySelector('#p2Button'),
+    display: document.querySelector('#p2Display'),
+    score:0
+}
+
 const p2Button = document.querySelector('#p2Button')
-const p1Display = document.querySelector('#p1Display')
-const p2Display = document.querySelector('#p2Display')
 const resetBtn = document.querySelector('#reset')
 const winningScoreSelect = document.querySelector('#playto')
 // initial Score
-let p1Score = 0
-let p2Score = 0
 let winningScore = 3
 let gameOver = false
 
 
-// Player One
-p1Button.addEventListener('click', () => {
+function updateScore(player,opponent){
     if (!gameOver) {
-        p1Score++
-        if (p1Score === winningScore) {
+        player.score++
+        if (player.score === winningScore) {
             gameOver = true
-            p1Button.classList.add('is-light')
-            p2Button.classList.add('is-light')
-            p1Button.disabled = true
-            p2Button.disabled = true
+            player.button.classList.add('is-light')
+            opponent.button.classList.add('is-light')
+            player.button.disabled = true
+            opponent.button.disabled = true
         }
-        p1Display.textContent = p1Score
+        player.display.textContent = player.score
 
     }
+}
+
+// Player One
+p1Button.addEventListener('click', () => {
+    updateScore(p1,p2)
+    
 })
 
 // Player Two
 p2Button.addEventListener('click', () => {
-    if (!gameOver) {
-        p2Score++
-        if (p2Score === winningScore) {
-            gameOver = true
-            p1Button.classList.add('is-light')
-            p2Button.classList.add('is-light')
-            p1Button.disabled = true
-            p2Button.disabled = true
-        }
-        p2Display.textContent = p2Score
-
-    }
+    updateScore(p2,p1)
 
 })
 
